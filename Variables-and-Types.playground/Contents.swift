7348cc071543dfaf0e123f590943e03db7b0eb54
Dustin McGuire <mcguire.dustin@att.net>
2020-04-27 18:55:22 -0500

    Udacity

---
+++ Variables-and-Types.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+Course Sandbox Learning how to use Xcode
+ */
+
+print("Hello from the IOS Team!!")
+
+print((200*7)/2)
+/*
+Introduction to swift
+ */
+var question = "Do you think you can learn Swift"
+print(question)
+
+var response = "You bet I can do it!"
+print(response)
+
+var question1 = "Does practice make perfect?"
+print(question1)
+
+var response1 = "Maybe not perfect but as close as possible"
+print(response1)
