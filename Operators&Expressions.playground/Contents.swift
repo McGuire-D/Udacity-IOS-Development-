8e937ef8060b87a49966537165dbfb67b7ff6b89
Dustin McGuire <mcguire.dustin@att.net>
2020-04-27 20:26:44 -0500

    Udacity

--- Operators&Expressions.playground/Contents.swift
+++ Operators&Expressions.playground/Contents.swift
@@ -23,3 +23,28 @@ var newTotalPoints = gamePoints + lifeBonus - deathBonus + randomBonus
 print(totalPoints)
 print(newTotalPoints)
 //: [Next](@next)|
+
+/*
+ Standard Operators in Swift
+ Below, we have created a table of some of the standard operators in Swift. Don’t worry if you do not understand the meaning of each operator yet because we will incorporate them more and more.
+
+ Here is an ordering of the precedence groups listed below from highest to lowest:
+
+ Multiplication
+ Addition
+ Assignment
+ Operators with a higher precedence group will be calculated before operators with a lower precedence group.
+
+ Operator    Name    Description    Precedence Group    Associativity    Type
+ *    Multiply    Performs basic multiplication    Multiplication    Left    Binary, Infix
+ /    Divide    Performs basic division (there are special rules governing division of integers and decimal values)    Multiplication    Left    Binary, Infix
+ %    Remainder    Performs basic division and returns the remainder that is left over    Multiplication    Left    Binary, Infix
+ +    Add    Performs basic addition    Addition    Left    Binary, Infix
+ -    Subtract    Performs basic subtraction    Addition    Left    Binary, Infix
+ =    Assignment    Assigns a value to a variable    Assignment    Right    Binary, Infix
+ +=    Add and assign    Performs + and then assigns the result to a variable    Assignment    Right    Binary, Infix
+ -=    Subtract and assign    Performs - and then assigns the result to a variable    Assignment    Right    Binary, Infix
+ *=    Multiply and assign    Performs * and then assigns the result to a variable    Assignment    Right    Binary, Infix
+ /=    Divide and assign    Performs / and then assigns the result to a variable    Assignment    Right    Binary, Infix
+ %=    Remainder and assign    Performs % and then assigns the result to a variable    Assignment    Right    Binary, Infix
+ */
