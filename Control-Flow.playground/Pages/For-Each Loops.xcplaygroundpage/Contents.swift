45d772cf661cfd8d15c17fb246270a4ff04ed85e
Dustin McGuire <mcguire.dustin@att.net>
2020-04-28 19:36:24 -0500

    Udacity

--- Control-Flow.playground/Pages/For-Each Loops.xcplaygroundpage/Contents.swift
+++ Control-Flow.playground/Pages/For-Each Loops.xcplaygroundpage/Contents.swift
@@ -22,3 +22,12 @@ for i in 0..<0 {
     print(i)
 }
 //: [Next](@next)
+
+let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+// your loop to print the characters in lorem goes here
+
+
+for c in lorem{
+    print(c)
+}
