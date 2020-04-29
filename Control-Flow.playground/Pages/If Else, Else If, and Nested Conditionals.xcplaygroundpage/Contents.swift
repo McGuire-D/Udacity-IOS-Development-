bdff69efa817500703afc7201f325bcec54895e2
Dustin McGuire <mcguire.dustin@att.net>
2020-04-28 19:13:16 -0500

    Udacity

--- Control-Flow.playground/Pages/If Else, Else If, and Nested Conditionals.xcplaygroundpage/Contents.swift
+++ Control-Flow.playground/Pages/If Else, Else If, and Nested Conditionals.xcplaygroundpage/Contents.swift
@@ -54,3 +54,61 @@ if hungry && vegetarian {
     print("Let's eat steak!")
 }
 //: [Next](@next)
+
+/*
+ The Club: Part 3
+ We know that if someone is on the guest list and is 21 or older, then they'll be admitted into the club. Otherwise, they will be turned away. Knowing this fact, rewrite your code into a single if statement. The else case should be used to turn the clubgoer away.
+ */
+
+// My attempt
+var name = "Stephen"
+var age = 8
+var onGuestList = true
+var knowsTheOwner = true
+
+if onGuestList && age >= 21 {
+    print("\(name), come party with us!")
+} else if !onGuestList || age < 21 {
+    print("Sorry, \(name), maybe you can go play Bingo with the Android team.")
+} else {
+    print("I am confused.")
+}
+
+// Redommended answer
+
+if onGuestList && age >= 21 {
+    print("\(name), come party with us!")
+} else {
+    print("Sorry, \(name), maybe you can go play Bingo with the Android team.")
+}
+
+
+/*
+ The Club: Part 4
+ There's one exception to our club's rules. If someone knows the owner, they can get into the party, regardless of their age or whether or not they're invited.
+
+ Add an else if statement to your program to check if knowsTheOwner is true. If so, print a message informing the person to see the owner.
+ */
+
+var name1 = "Stephen"
+var age1 = 8
+var onGuestList1 = true
+var knowsTheOwner1 = true
+
+
+if onGuestList1 && age1 >= 21 {
+    print("\(name1), come party with us!")
+} else if knowsTheOwner1 {
+    print("Oh you know the owner \(name1)? Why didnt you say so come in!")
+} else {
+    print("Sorry, \(name1), maybe you can go play Bingo with the Android team.")
+}
+
+// Recoommended answer
+if onGuestList && age >= 21 {
+    print("\(name), come party with us!")
+} else if knowsTheOwner {
+    print("\(name), we'll have to take you to see the owner.")
+} else {
+    print("Sorry, \(name), maybe you can go play Bingo with the Android team.")
+}
