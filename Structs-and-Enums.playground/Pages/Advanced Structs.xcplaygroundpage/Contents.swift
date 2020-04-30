4ca5e6520567c721a64adcc523dccc9047113e53
Dustin McGuire <mcguire.dustin@att.net>
2020-04-29 19:07:35 -0500

    Udacity

--- Structs-and-Enums.playground/Pages/Advanced Structs.xcplaygroundpage/Contents.swift
+++ Structs-and-Enums.playground/Pages/Advanced Structs.xcplaygroundpage/Contents.swift
@@ -48,3 +48,27 @@ let beaker1 = Beaker(volumeMilliliters: 500)
 let beaker2 = Beaker(volumeMilliliters: 1000)
 Beaker.madeIn = "Japan"
 //: [Next](@next)
+
+/*
+ Why Are Structs Useful?
+ Why are structs useful? And, can you give an example of when you would want to use one?
+ 
+ Things to think about
+ Structs are useful because they allow us to bind-up multiple types into one easy-to-use type. One situation where you might want to use a struct is to encapsulate all the properties associated with a book. Imagine you have an app that lets you search for a book by ISBN, and then displays the book's information.
+ */
+
+struct Band{
+    let name: String
+    let genre: String
+    let members: BandMembers
+    }
+
+struct BandMembers{
+    let members: String
+}
+
+let crew = BandMembers(members:"Benjamin Burnley, Aaron Bruch, Shaun Foist, Jasen Rauch, Keith Wallen")
+
+let BreakingBenjamin = Band(name: "Breaking Benjamin", genre: "Rock", members: crew)
+
+print(BreakingBenjamin)
