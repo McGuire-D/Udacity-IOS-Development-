f168540fda9b2e2a8b90a2a4bbd7743152843fb2
Dustin McGuire <mcguire.dustin@att.net>
2020-04-29 19:24:55 -0500

    Udacity

--- Structs-and-Enums.playground/Pages/Enums.xcplaygroundpage/Contents.swift
+++ Structs-and-Enums.playground/Pages/Enums.xcplaygroundpage/Contents.swift
@@ -16,3 +16,24 @@ enum AmericanLeagueWest {
     case rangers
 }
 //: [Next](@next)
+
+/*
+ Practice: Finger Enum
+ Write an enum called Finger to represent the five fingers on a human hand. Use the following cases.
+
+ thumb
+ index
+ middle
+ ring
+ pinky
+ Assign a raw value of type Int to each finger, counting from 1 through 5. For example thumb should have a raw value of 1, index, a raw value of 2 and so on. Use the exact case names as above.
+*/
+ enum Finger: Int {
+     case thumb = 1
+     case index = 2
+     case middle = 3
+     case ring = 4
+     case pinky = 5
+ }
+
+
