ba6611e210cdf1312a1b7bb883088d5b5e1cb5ce
Dustin McGuire <mcguire.dustin@att.net>
2020-04-30 19:17:52 -0500

    Udacity

--- Optionals.playground/Pages/Unsafe Unwrapping and Implicitly Unwrapped Optionals.xcplaygroundpage/Contents.swift
+++ Optionals.playground/Pages/Unsafe Unwrapping and Implicitly Unwrapped Optionals.xcplaygroundpage/Contents.swift
@@ -20,3 +20,20 @@ villain.evilScheme = "steal from the cookie jar"
 // we know for sure the scheme has been set
 villain.performScheme()
 //: [Next](@next)
+
+/*
+ Reflect
+ What is the difference between unwrapping an optional with ?, with ! and with implicit unwrapping?
+
+
+ 
+ 
+ Things to think about
+ Thanks for sharing. Here's what we answered.
+
+ Unwrapping with ? is useful if you're accessing nested properties or want some default behavior to occur if a value is nil.
+
+ Using ! does not require providing a default value and is useful if you know that a value won't be nil. However, if it is nil, the program will crash.
+
+ Implicitly unwrapping optionals is also useful when you know a value won't be nil, but you can use the same syntax as a normal property. However, unintended behavior and crashes can occur if it is nil.
+ */
