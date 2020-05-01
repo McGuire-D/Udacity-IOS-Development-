527030e8ce0379d521dac2ed6eca9413e98893f6
Dustin McGuire <mcguire.dustin@att.net>
2020-04-30 19:36:06 -0500

    Udacity

--- Strings.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Strings.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -6,3 +6,19 @@ print("Hello, world!")
 var myFavoriteAnimal = "nudibranch"
 let encouragement = "You can do it!"
 //: [Next](@next)
+
+
+/*
+Reflect
+While this lesson explores Swift strings in-depth, this is not the first time you've encountered this data type. What techniques have you learned about working with strings so far?
+
+ Your reflection
+ Strings are capable of holding many things, from letter, numbers and symbols.
+ Things to think about
+ Thanks for sharing! Here are some ways we've used string so far.
+
+ Interpolation - we can insert values into strings when using print() to produce readable output.
+ Concatenation - we can add strings together to produce a combined string
+ Loops - we can get each individual character in a string using for in syntax (e.g. for c in someString)
+ 
+ */
