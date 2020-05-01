315f6ead31b8e85dc818a9a6a3124a59dba8e2fb
Dustin McGuire <mcguire.dustin@att.net>
2020-04-30 19:47:02 -0500

    Udacity

--- Strings.playground/Pages/Properties and Methods.xcplaygroundpage/Contents.swift
+++ Strings.playground/Pages/Properties and Methods.xcplaygroundpage/Contents.swift
@@ -17,3 +17,24 @@ str.lowercased()
 str.uppercased()
 String(str.reversed())
 //: [Next](@next)
+
+/*
+ Practice: Spam Filter
+ As part of the spam filter for a message board app, there's a function called checkLength(). The goal of this function is to prevent posts that are too short (less than 10 characters) and prevent posts that are too long (more than 10,000 characters). Finish implementing the function to return true if a message is within the length restrictions and false if it is too long or too short.
+ */
+
+import Foundation
+
+func checkLength(message: String) -> Bool {
+    return message.count <= 10000 && message.count >= 10
+}
+
+/*
+ Practice: Case Insensitive Search
+ Finish implementing search(). If the second string (s2) occurs anywhere in the first string (s1), return true. Otherwise, return false. The search should be case insensitive. For example, search(s1: "Udacity", s2: "CITY") returns true.
+
+ */
+
+func search(s1: String, s2: String) -> Bool {
+    return s1.lowercased().range(of: s2.lowercased()) != nil
+}
