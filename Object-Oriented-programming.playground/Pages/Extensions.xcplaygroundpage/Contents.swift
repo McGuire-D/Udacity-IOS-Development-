43782482488af081276476f30e8f2dcf584e7faf
Dustin McGuire <mcguire.dustin@att.net>
2020-05-02 12:53:06 -0500

    Udacity

--- Object-Oriented-programming.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
+++ Object-Oriented-programming.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -32,4 +32,26 @@ Thanks for sharing! Here's what we answered.
 Something that is passed by value such as an instance of a struct is copied when it's assigned to a variable or passed to a function. Changing a property on the instance will only apply to the copy, and the original instance will remain unchanged.
 
 Values that are passed by reference, such as instances of classes, are not copied. Changing a value on a copy will also modify the original instance, since the copy is simply referencing the original.
-*/
+
+ 
+ Reflect
+ For each of the following, determine if it's best represented by a class, property, or method.
+
+ 1. The tail length of an animal.
+ 2. Baking cookies
+ 3. An employee
+ 4. A vehicle belonging to a person
+ 5. Sending an email
+
+ 
+ I actually answered this correctly 🏅 using udacity's answer as it goes into more detail.
+ 
+ Things to think about
+ Here's what we answered.
+
+ 1.Property: Tail length is a simple numeric value that can be represented by an integer or floating point number.
+ 2.Method: Baking cookies is an action, so a method would be used.
+ 3.Class: An employee may have many properties and also has the ability to perform actions, so this sounds like a class.
+ 4.Property: The vehicle belongs to a person, so it is likely the property of a class representing a person. If you answered "Class" than that's right too, as it has many properties and performs many functions.
+ 5.Method: This is also an action, and methods perform actions.
+ */
