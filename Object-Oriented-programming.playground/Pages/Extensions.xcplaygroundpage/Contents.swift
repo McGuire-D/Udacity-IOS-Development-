0678564ac67f0c7b065957a3f2200629a0ea3e4e
Dustin McGuire <mcguire.dustin@att.net>
2020-05-02 12:50:31 -0500

    Udacity

--- Object-Oriented-programming.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
+++ Object-Oriented-programming.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -24,3 +24,12 @@ extension Roommate: Souschef {
         return "The \(vegetable) is so fresh and so clean"
     }
 }
+
+/*
+Things to think about
+Thanks for sharing! Here's what we answered.
+
+Something that is passed by value such as an instance of a struct is copied when it's assigned to a variable or passed to a function. Changing a property on the instance will only apply to the copy, and the original instance will remain unchanged.
+
+Values that are passed by reference, such as instances of classes, are not copied. Changing a value on a copy will also modify the original instance, since the copy is simply referencing the original.
+*/
