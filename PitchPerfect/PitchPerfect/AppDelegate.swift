68bcce4e271b93a81dc8d9c74db47b7348be9721
Dustin McGuire <mcguire.dustin@att.net>
2020-05-04 19:24:18 -0500

    UdacityProject

--- PitchPerfect/PitchPerfect/AppDelegate.swift
+++ PitchPerfect/PitchPerfect/AppDelegate.swift
@@ -9,13 +9,12 @@
 import UIKit
 
 @UIApplicationMain
-
-
-
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-     // Removed Hello World! 
+        // Override point for customization after application launch.
         return true
     }
 
@@ -35,7 +34,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-
-
-
 
