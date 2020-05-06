c91ef328d339d4b2ded0fd23044d986f07bc402f
Dustin McGuire <mcguire.dustin@att.net>
2020-05-06 18:21:39 -0500

    Udacity

--- PitchPerfect/PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/PitchPerfect/RecordSoundsViewController.swift
@@ -85,4 +85,12 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate  {
 
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! playSoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordAudioURL = recordedAudioURL
+        }
+    }
+    
 }
