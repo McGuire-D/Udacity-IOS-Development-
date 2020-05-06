0e231f9e14a08129bd513bd3c33296fa900b5519
Dustin McGuire <mcguire.dustin@att.net>
2020-05-05 19:26:57 -0500

    Udacity

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
     @IBAction func RecordAudio(_ sender: Any) {
         print("Record button was pressed")
+        recordingLabel.text="Recording in Progress"
     }
     
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Button pressed")
+    }
 }
 
