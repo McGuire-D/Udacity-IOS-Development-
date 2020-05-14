b631819c0a7c5d8aed4f2c477626fbf7625fd2a6
Dustin McGuire <mcguire.dustin@att.net>
2020-05-14 18:09:27 -0500

    Udacity

---
+++ Click Counter/Click Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Dustin McGuire on 5/14/20.
+//  Copyright © 2020 Dustin McGuire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
