31b360ec7bd4d154de7c92b1093ac98977b2160d
Dustin McGuire <mcguire.dustin@att.net>
2020-05-14 18:43:35 -0500

    Udacity

--- Click Counter/Click Counter/ViewController.swift
+++ Click Counter/Click Counter/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     
     var count = 0
     var label : UILabel!
-
+    var label2 : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,6 +24,13 @@ class ViewController: UIViewController {
         view.addSubview(label)
         self.label = label
         
+        // label 2
+        let label2 = UILabel()
+        label2.frame = CGRect(x: 150, y: 40, width: 60, height: 60)
+        label2.text = "0"
+        view.didAddSubview(label2)
+        self.label2 = label2
+        
         //button
         let button = UIButton()
         button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
@@ -32,11 +39,21 @@ class ViewController: UIViewController {
         view.addSubview(button)
         
         button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+        
+        
     }
     
     @objc func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+        
+    func countDown() {
+        self.count -= 1
+        self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+            
+        }
     }
 
 }
