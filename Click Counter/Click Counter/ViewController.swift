fc67c167746fa80ed503d183a5413e07d686fa93
Dustin McGuire <mcguire.dustin@att.net>
2020-05-14 19:03:38 -0500

    Udacity

--- Click Counter/Click Counter/ViewController.swift
+++ Click Counter/Click Counter/ViewController.swift
@@ -24,37 +24,47 @@ class ViewController: UIViewController {
         view.addSubview(label)
         self.label = label
         
-        // label 2
+        // label 2 testing adding my own label
         let label2 = UILabel()
-        label2.frame = CGRect(x: 150, y: 40, width: 60, height: 60)
+        label2.frame = CGRect(x: 150, y: 90, width: 60, height: 60)
         label2.text = "0"
-        view.didAddSubview(label2)
+        view.addSubview(label2)
         self.label2 = label2
         
         //button
         let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
+        button.frame = CGRect(x: 150, y: 250, width: 100, height: 60)
+        button.setTitle("Count Up", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
         
         button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
         
+        //button 2 practice
+        let button2 = UIButton()
+        button2.frame = CGRect(x: 150, y: 220, width: 100, height: 60)
+        button2.setTitle("Count Down", for: .normal)
+        button2.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button2)
+        
+        button2.addTarget(self, action: #selector(ViewController.countDown), for: UIControl.Event.touchUpInside)
         
     }
-    
+    // function to count up by 1
     @objc func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
         self.label2.text = "\(self.count)"
-        
-    func countDown() {
+    }
+    
+    // function to count down by 1
+    @objc func countDown() {
         self.count -= 1
         self.label.text = "\(self.count)"
         self.label2.text = "\(self.count)"
             
-        }
     }
+    
 
 }
 
