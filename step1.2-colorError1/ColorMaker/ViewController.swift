c6a7175d123d0fb690fd03c1f8407f797fb0aff3
Dustin McGuire <mcguire.dustin@att.net>
2020-05-16 09:18:28 -0500

    Udacity

---
+++ step1.2-colorError1/ColorMaker/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by Jason Schatz on 11/2/14.
+//  Copyright (c) 2014 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redControl: UISwitch!
+    @IBOutlet weak var greenControl: UISwitch!
+   // This was an easy fix the blue control was missing a connection to the IBAction
+    @IBOutlet weak var blueControl: UISwitch!
+
+    // MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        changeColorComponent()
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func changeColorComponent() {
+        
+        if self.redControl == nil {
+            return
+        }
+        
+        let r: CGFloat = self.redControl.isOn ? 1 : 0
+        let g: CGFloat = self.greenControl.isOn ? 1 : 0
+        let b: CGFloat = self.blueControl.isOn ? 1 : 0
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
