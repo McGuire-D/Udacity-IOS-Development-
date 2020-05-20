ecd239b320cf26982edf99269a45b4aeab7526e1
Dustin McGuire <mcguire.dustin@att.net>
2020-05-19 19:01:26 -0500

    Udacity

--- OneButtonExpirement/OneButtonExpirement/ViewController.swift
+++ OneButtonExpirement/OneButtonExpirement/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController : UIViewController, UIImagePickerControllerDelegate, UINavi
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+// camera action function
     @IBAction func cameraButton(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             let imagePicker = UIImagePickerController()
@@ -26,7 +26,7 @@ class ViewController : UIViewController, UIImagePickerControllerDelegate, UINavi
             present(imagePicker, animated: true, completion: nil)
         } 
     }
-    
+// library action function to access phone library
     @IBAction func libraryButton(_sender: Any)  {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             let imagePicker = UIImagePickerController()
@@ -34,13 +34,32 @@ class ViewController : UIViewController, UIImagePickerControllerDelegate, UINavi
             imagePicker.sourceType = .photoLibrary;
             imagePicker.allowsEditing = false
             present(imagePicker, animated: true, completion: nil)
+        let controller = UIAlertController()
+        present(controller, animated: true, completion: nil)
         }
     }
 
+// test for Alert button
+    @IBAction func experiment() {
+        let controller = UIAlertController()
+        
+        controller.title = "Test Alert"
+        controller.message = "This is a test"
+        
+        let okAction = UIAlertAction(title: "ok", style: UIAlertAction.Style.default) { action in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        controller.addAction(okAction)
+        present(controller, animated: true, completion: nil)
+        
+    }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             myImage.image = image
+            
+            
         }
         dismiss(animated: true, completion: nil)
         }
