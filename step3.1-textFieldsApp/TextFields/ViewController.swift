260cf21e1c08528244bd148729f45cdcb1cdd566
Dustin McGuire <mcguire.dustin@att.net>
2020-05-26 08:47:56 -0500

    Udacity

--- step3.1-textFieldsApp/TextFields/ViewController.swift
+++ step3.1-textFieldsApp/TextFields/ViewController.swift
@@ -37,19 +37,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: Text Field Delegate Methods
-    
+
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
         // Figure out what the new text will be, if we return true
         var newText = textField.text! as NSString
         newText = newText.replacingCharacters(in: range, with: string) as NSString
-        
+
         // hide the label if the newText will be an empty string
         self.characterCountLabel.isHidden = (newText.length == 0)
-        
+
         // Write the length of newText into the label
         self.characterCountLabel.text = String(newText.length)
-        
+
         // returning true gives the text field permission to change its text
         return true;
     }
