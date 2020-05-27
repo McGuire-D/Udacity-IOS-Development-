532b1bce42e829d29bef93dab6489704786f7c3c
Dustin McGuire <mcguire.dustin@att.net>
2020-05-27 09:43:58 -0500

    Udacity

---
+++ Text-Fields-Challenge-App/Text-Fields-Challenge-App/Delegates/cashTextField.swift
@@ -0,0 +1,75 @@
+//
+//  CashTextField.swift
+//  Text-Fields-Challenge-App
+//
+//  Created by Dustin McGuire on 5/27/20.
+//  Copyright © 2020 Dustin McGuire. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CashTextField : NSObject, UITextFieldDelegate {
+
+    /// The numbers that have been entered in the text field
+    private var enteredNumbers = ""
+
+    private var didBackspace = false
+
+    var locale: Locale = .current
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        addTarget(self, action: #selector(editingChanged), for: .editingChanged)
+    }
+
+    override func deleteBackward() {
+        enteredNumbers = String(enteredNumbers.dropLast())
+        text = enteredNumbers.asCurrency(locale: locale)
+        // Call super so that the .editingChanged event gets fired, but we need to handle it differently, so we set the `didBackspace` flag first
+        didBackspace = true
+        super.deleteBackward()
+    }
+
+    @objc func editingChanged() {
+        defer {
+            didBackspace = false
+            text = enteredNumbers.asCurrency(locale: locale)
+        }
+
+        guard didBackspace == false else { return }
+
+        if let lastEnteredCharacter = text?.last, lastEnteredCharacter.isNumber {
+            enteredNumbers.append(lastEnteredCharacter)
+        }
+    }
+}
+
+private extension Formatter {
+    static let currency: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }()
+}
+
+private extension String {
+    func asCurrency(locale: Locale) -> String? {
+        Formatter.currency.locale = locale
+        if self.isEmpty {
+            return Formatter.currency.string(from: NSNumber(value: 0))
+        } else {
+            return Formatter.currency.string(from: NSNumber(value: (Double(self) ?? 0) / 100))
+        }
+    }
+}
