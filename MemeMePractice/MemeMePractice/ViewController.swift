8265c7dfba0fe636c414616e4a32faf99199017a
Dustin McGuire <mcguire.dustin@att.net>
2020-06-04 20:24:55 -0500

    Udacity

--- MemeMePractice/MemeMePractice/ViewController.swift
+++ MemeMePractice/MemeMePractice/ViewController.swift
@@ -8,53 +8,68 @@
 
 import UIKit
 
+extension UIViewController {
+    public func addActionSheetForiPad(actionSheet: UIAlertController) {
+        if let popoverPresentationController = actionSheet.popoverPresentationController {
+            popoverPresentationController.sourceView = self.view
+            popoverPresentationController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.maxY, width: 0, height: 0)
+            popoverPresentationController.permittedArrowDirections = []
+        }
+    }
+}
+
+extension UIViewController {
+    public func addActivityViewControllerForiPad(activityController: UIActivityViewController) {
+        if let popoverPresentationController = activityController.popoverPresentationController {
+            popoverPresentationController.sourceView = self.view
+            popoverPresentationController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.maxY, width: 0, height: 0)
+            popoverPresentationController.permittedArrowDirections = []
+        }
+    }
+}
+
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
     
     
     @IBOutlet weak var toodleBar: UIToolbar!
     @IBOutlet weak var topText: UITextField!
     @IBOutlet weak var bottomText: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancel: UIBarButtonItem!
     
     var mainFont: UIFont?
     
     let memeTextAttributes: [NSAttributedString.Key: Any] = [
           .strokeColor: UIColor.purple,
-          .foregroundColor: UIColor.white,
           .font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-          .strokeWidth: 2,
+          .strokeWidth: -4,
+          .foregroundColor: UIColor.white,
           ]
     
     @IBOutlet weak var imagePickerView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         toodleBar.isHidden = false
-        topText.textAlignment = .center
-        bottomText.textAlignment = .center
         topText.defaultTextAttributes = memeTextAttributes
         bottomText.defaultTextAttributes = memeTextAttributes
         topText.delegate = self
         bottomText.delegate = self
+        topText.textAlignment = .center
+        bottomText.textAlignment = .center
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        topText.backgroundColor = .systemPink
-        bottomText.backgroundColor = .systemPink
+        topText.backgroundColor = .clear
+        bottomText.backgroundColor = .clear
+        topText.borderStyle = .none
+        bottomText.borderStyle = .none
+        shareButton.isEnabled = false
     }
-    /*@objc func keyboardWillShow(notification: NSNotification){
-        print("Keyboard Will Show")
-    }
- */
+  
     @objc func keyboardWillHide(notification: NSNotification){
         print("Keyboard hidden")
         view.frame.origin.y = 0
     }
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        subscribeToKeyboardNotifications()
-//    }
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        unSubscribeToKeyboardNotifications()
-//    }
+
     func textFieldDidBeginEditing(_ textField: UITextField) {
         textField.text = nil
     }
@@ -65,14 +80,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return true;
     }
     
-   /* func subscribeToKeyboardNotifications () {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-    }
-    
-    func unSubscribeToKeyboardNotifications () {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-    }
-    */
     @objc func keyboardWillShow(_ notification:Notification) {
         view.frame.origin.y -= getKeyboardHeight(notification)
         print("Keyboard Will Show")
@@ -108,14 +115,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(pickerController, animated: true, completion: nil)
         
     }
+    @IBAction func share(_ sender: Any) {
+        let image = generateMemedImage()
+        let ac = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        ac.addActivityViewControllerForiPad(activityController: ac)
+        present(ac, animated: true)
+    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.editedImage] as? UIImage {
             imagePickerView.image = image
+            shareButton.isEnabled = true
         }
         imagePickerView.setNeedsDisplay()
         dismiss(animated: true)
 
     }
     
+    func save() {
+        _ = Meme(topText: topText.text!, bottomText: bottomText.text!, originalImage: imagePickerView.image!, editedImage: generateMemedImage())
+    }
+    
+    func generateMemedImage() -> UIImage{
+        
+    
+        // TODO: Hide toolbar and navbar
+        
+        // Render view to an Image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let editedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        //TODO: Show tollbar and navbar
+        
+        return editedImage
+    }
+    
+}
+// struct for meme image
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var editedImage: UIImage
 }
