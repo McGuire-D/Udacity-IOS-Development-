aedb83553645d52473fd21f30145b26b5d4909f1
Dustin McGuire <mcguire.dustin@att.net>
2020-09-22 20:25:25 -0500

    Udacity

--- Saving APOD/Saving APOD/ViewController.swift
+++ Saving APOD/Saving APOD/ViewController.swift
@@ -8,13 +8,173 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// code to allow Textbox to have other attributes (bold, italics, normal)
+// https://stackoverflow.com/questions/28496093/making-text-bold-using-attributed-string-in-swift/37992022
+extension NSMutableAttributedString {
+    @discardableResult func bold(_ text: String) -> NSMutableAttributedString { // note bold is for title
+        let attrs:  [NSAttributedString.Key: Any] = [.font: UIFont.boldSystemFont(ofSize: 20)]
+        let boldString = NSMutableAttributedString(string:text, attributes: attrs)
+        self.append(boldString)
+        
+        return self
+    }
+    
+    @discardableResult func italics(_ text: String) -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [.font: UIFont.italicSystemFont(ofSize: 16)]
+        let boldString = NSMutableAttributedString(string:text, attributes: attrs)
+        self.append(boldString)
+        
+        return self
+    }
+    
+    @discardableResult func normal(_ text: String) -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 16)]
+        let normal = NSAttributedString(string: text, attributes: attrs)
+        self.append(normal)
+        
+        return self
+    }
+    
+    @discardableResult func newline() -> NSMutableAttributedString {
+        let attrs: [NSAttributedString.Key: Any] = [.font: UIFont.systemFont(ofSize: 16)]
+        let normal = NSAttributedString(string: "\n\n", attributes: attrs)
+        self.append(normal)
+        
+        return self
+    }
+}
 
+class ViewController: UIViewController, datePickerDelegate {
+    let apiKey : String = "DEMO_KEY" // replace API key with https://api.nasa.gov/index.html#apply-for-an-api-key
+
+    @IBOutlet weak var ViewImage: UIImageView!
+    @IBOutlet weak var TextBox: UITextView!
+    
+    struct photoInfo: Codable {
+        var date: String
+        var title: String
+        var description: String
+        var url: URL
+        var copyright: String?
+        enum CodingKeys: String, CodingKey {
+            case date
+            case title
+            case description  = "explanation"
+            case url
+            case copyright
+        }
+    }
+    
+    @IBAction func APODTest(_ sender: Any) {
+        performSegue(withIdentifier: "pickDate", sender: self)
+    }
+    
+    func dateChanged(date: Date) {
+        convertData(date: date)
+    }
+    
+    private func requestAPOD(date : Date, callback: @escaping (photoInfo, UIImage) -> Void ) -> Void {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        
+        let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&date=" + dateFormatter.string(from: date))!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if (self.errorHandlerResp(data: data, response: response, error: error)) {
+                let jsonDecoder = JSONDecoder()
+                //let leftdata = data!
+                if let photoInfo = try? jsonDecoder.decode(photoInfo.self, from: data!) {
+                    let task2 = URLSession.shared.dataTask(with: photoInfo.url) { (data, response, error) in
+                        if (self.errorHandlerResp(data: data, response: response, error: error)) {
+                            callback(photoInfo, UIImage(data: data!)!)
+                        }
+                    }
+                    task2.resume()
+                }
+            }
+        }
+        task.resume()
+    }
+    
+
+    public func convertData(date: Date) -> Void {
+        requestAPOD(date: date, callback: {(info: photoInfo?, image: UIImage?) in
+            DispatchQueue.main.async {
+                let formattedString = NSMutableAttributedString()
+                if info != nil {
+                    if image != nil {
+//                        let ratio = Float((image?.size.width)!) / Float((image?.size.height)!)
+//                        let width = self.ViewImage.frame.width
+//                        let calcHeight = Float(self.ViewImage.frame.width) * ratio
+//                        self.ViewImage.frame = CGRect(x: 0.0, y: 0.0, width: Double(width), height: Double(calcHeight))
+                        self.ViewImage.image = image
+                    } else {
+                        self.ViewImage.image = nil
+                    }
+                    formattedString.bold((info?.title)!).newline().normal((info?.description)!).newline()
+                    if info?.copyright == nil {
+                        formattedString.italics("Copyright: Public Domain")
+                    } else {
+                        formattedString.italics("Copyright: " + (info?.copyright)!)
+                    }
+                } else {
+                formattedString.bold("No Title").newline().normal("No Description")
+                }
+                self.TextBox.attributedText = formattedString
+                self.TextBox.textColor = UIColor.white
+            }
+        })
+    } // end convertData
+    
+    private func errorHandlerResp(data: Data?, response: URLResponse?, error: Error?) -> Bool {
+        let httpResponse = response as! HTTPURLResponse
+        let title = "NASA APOD Error"
+        guard httpResponse.statusCode == 200 else {
+            if httpResponse.statusCode == 429 {
+                alertControl(title: title, message: "APIkey cannot currently handle any more responses...")
+            } else { // key is dead
+                alertControl(title: title, message: "Unexpected httpResponse code...")
+            }
+            return false
+        }
+        guard data != nil else {
+            alertControl(title: title, message: "No Entry for this day, try another day.")
+            return false
+        }
+        guard error == nil else {
+            alertControl(title: title, message: "Unexpected Error.")
+            return false
+        }
+        return true
+    }
+    
+    func alertControl(title: String, message: String) -> Void {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        convertData(date: Date.init())
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? datePickerViewController {
+            vc.delegate = self
+            
+        }
+    }
 }
 
---
+++ Saving APOD/Saving APOD/datePickerViewController.swift
@@ -0,0 +1,53 @@
+//
+//  datePickerViewController.swift
+//  Saving APOD
+//
+//  Created by Dustin McGuire on 9/16/20.
+//  Copyright © 2020 Dustin McGuire. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol datePickerDelegate: class {
+    func dateChanged(date: Date)
+    
+}
+
+class datePickerViewController: UIViewController {
+    @IBOutlet weak var APODdate: UIDatePicker!
+    weak var delegate: datePickerDelegate?
+    var returnDate : Date? = Date.init()
+    
+    @IBAction func APODChanged(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        
+        
+
+        returnDate = APODdate.date
+    }
+    
+    @IBAction func OkAPODDatePicker(_ sender: Any) {
+        delegate?.dateChanged(date: returnDate!)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelAPODDatePicker(_ sender: Any) {
+        dismiss(animated: true, completion: nil) 
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        APODdate.maximumDate = Date()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
